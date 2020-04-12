2f90aadf7ed28e7c17cf38cc6497b1868bbd719c
Mitchasignatura <cecilcastroverde81@gmail.com>
2020-04-12 23:49:19 +0800

    updated SITE URL

--- conf.py
+++ conf.py
@@ -20,7 +20,7 @@ BLOG_AUTHOR = "Maria Cecilia Castroverde"  # (translatable)
 BLOG_TITLE = "Asignatura"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
-SITE_URL = "https://example.com/"
+SITE_URL = "https://mitchasignatura.github.io"
 # This is the URL where Nikola's output will be deployed.
 # If not set, defaults to SITE_URL
 # BASE_URL = "https://example.com/"
